aa1561f60a9f92e9043f444f3cd96c46a6742af7
Spuriosity1 <alaric.sanders@gmail.com>
2024-12-28 15:24:34 +0000

    somewhat successful implementation

--- src/gen_spinon_basis.cpp
+++ src/gen_spinon_basis.cpp
@@ -1,167 +1,67 @@
-#include <bitset>
-#include <nlohmann/json_fwd.hpp>
-#include <queue>
-#include <fstream>
-#include <iostream>
-#include <map>
-#include <strings.h>
-#include <utility>
-#include <vector>
-#include <set>
-#include "tetra_graph_io.hpp"
 #include "bittools.hpp"
+#include "pyro_tree.hpp"
+#include <cstdio>
+#include <fstream>
+#include <nlohmann/json.hpp>
+#include <nlohmann/json_fwd.hpp>
+
+
+
+
 
 using namespace std;
 using json=nlohmann::json;
 
-template<typename T>
-inline void setbits(__int128 &x, T &spin_idx) {
-	for (auto J : spin_idx) {
-		x |= (1 << J);
+void print_tetra(const spin_set& t){
+		printf("tetra at %p members [", static_cast<const void*>(&t) );
+		for (auto si : t.member_spin_ids){printf( "%d, ", si);}
+		printf("] bitmask ");
+		auto b = t.bitmask;
+		printf("0x%016llx%016llx\n", b.uint64[1],b.uint64[0]);
+}
+
+void print_spin(int idx, const spin& s){
+	printf("Spin %d at %p ", idx, static_cast<const void*>(&s));
+	printf("Neighbours:\n");
+	for (auto t : s.tetra_neighbours){
+		printf("\t");
+		print_tetra(*t);
 	}
 }
 
-
-template<typename T>
-inline void setbits(__int128 &x, const T &spin_idx, bitset<64> bits) {
-	int i=0;
-
-	for (auto J : spin_idx) {
-		x |= (bits[i] << J);
-		i++;
-	}
-}
-
-const static std::array<int, 6> states_2I2O = {0b0011, 0b0101, 0b1001,
-	0b1100, 0b1010, 0b0110};
-
-
-struct tetra_tree_node {
-	int idx;
-	tetra_tree_node* prev = nullptr;
-	// pairs of the form spin_idx, tetra_tree_node*
-	// these are the free spins
-	std::vector<std::pair<int, tetra_tree_node *>> leaves; 
-														   
-														   
-	__int128 constraint_mask; // a mask such that statedict[state & constraint_mask] gives all accessible states
-	std::map<__int128, std::vector<__int128>> statedict;
-};
-
-struct tetra_tree {
-	tetra_tree(const tetra_graph& g, int root_idx=0) {
-		root = new tetra_tree_node;
-		root->idx = root_idx;
-
-		// keep track of which spins we have covered
-		std::set<int> g_owned_spin_ids;
-
-
-		std::queue<std::pair<tetra*, tetra_tree_node*>> next;
-		next.push(std::make_pair(g.tetra_no[0], root));
-		
-		index.resize(g.tetra_no.size());
-		while (next.size() > 0){
-			auto [curr_tet, curr_node] = next.front();
-			next.pop();
-			for (const auto [si, s_tetra] : curr_tet->links){
-				if (g_owned_spin_ids.insert(si).first == g_owned_spin_ids.end()){
-					// unsuccessful insertion. si already owned by another tetra
-					continue;
-				}
-				// si has not yet been traversed, we own this spin now
-				tetra_tree_node* tn = new tetra_tree_node;
-				tn->idx = s_tetra->idx;
-				tn->prev = curr_node;
-
-				curr_node->leaves.push_back(std::make_pair(si, tn));
-				next.push(std::make_pair(s_tetra,tn));
-				index[tn->idx] = tn;
-			}
-		}
-		assert(g_owned_spin_ids.size() < g.num_spins());
-		set_lookup_tables(g);
-
-
+int main (int argc, char *argv[]) {
+	if (argc < 2) {
+		printf("USAGE: %s <latfile: json\n", argv[0]);
 	}
 
-	inline std::vector<int> free_spin_ids(){
-		// Returns a set of all the free spins
-		std::vector<int> res;
-		for (auto t: this->index){
-			for (auto [si, _] : t->leaves){
-				res.push_back(si);
-			}
-		}
-		return res;
+	std::string infilename(argv[1]);
+	string outfilename=infilename.substr(0,infilename.find_last_of('.'))+".csv";
+
+	ifstream ifs(infilename);
+	json data = json::parse(ifs);
+	ifs.close();
+
+	pyro_tree L(data);
+
+	printf("Building state tree...");
+	L.build_state_tree();
+	
+#if VERBOSITY > 3
+	for (auto t: L.tetras){	print_tetra(t); }
+#endif
+#if VERBOSITY > 2
+	for (int si=0; si<L.spins.size(); si++){
+		print_spin(si, L.spins[si]);
 	}
+	L.print_state_tree();
+#endif
+	
 
-
-	void enumerate_ice_states(std::vector<__int128>& out){
-
-		// loose idea of the algorithm:
-		// 1. starting from the root (least constrained), choose a free state
-		std::queue<tetra_tree_node*> next;
-		__int128 curr_state;
-		next.push(this->root);
-		auto free_spins = pruned_tree();
-
-
-
+	FILE* outfile = std::fopen(outfilename.c_str(), "w");
+	for (Uint128 b : L.get_states()){
+		std::fprintf(outfile, "0x%016llx%016llx\n", b.uint64[1],b.uint64[0]);
 	}
+	std::fclose(outfile);
 
-private:
-	void set_lookup_tables(const tetra_graph& g){
-		// set the state-lookup tables
-		for (int i=0; i<index.size(); i++){
-			tetra_tree_node* node = index[i];
-			tetra* tetra = g.tetra_no[i];
-
-			std::vector<int> free_spins;
-			std::set<int> constr_spins;
-			for (auto [spini, ti] : tetra->links){
-				// all spins connected 
-				constr_spins.insert(spini);
-			}
-			for (auto [spini, nodei] : node->leaves){
-				constr_spins.erase(spini);
-				free_spins.push_back(spini);
-			}
-			setbits(node->constraint_mask,constr_spins);
-			// now, node's constraint mask is correctly set.
-			for (int i=0; i<(1<<tetra->links.size()); i++){
-				// understood as i = 0     |  0 0 0
-				//                   free  | constr
-				int free_substate = i >> constr_spins.size();
-				int constr_substate = i ^ (free_substate << constr_spins.size());
-				__int128 free_state=0;
-				__int128 constr_state=0;
-				setbits(free_state, free_spins, free_substate);
-				setbits(constr_state, constr_spins, constr_substate);
-				if (node->statedict.find(constr_state) == node->statedict.end()){
-					node->statedict[constr_state] = {};
-				}
-				node->statedict[constr_state].push_back(free_state);
-			}	
-		}
-	}
-	std::vector<tetra_tree_node*> index;
-	tetra_tree_node* root;
-};
-
-int main(int argc, char *argv[]) {
-  if (argc < 2) {
-    cout << "USAGE: " << argv[0] << "<latfile: json>";
-  }
-  ifstream ifs(argv[1]);
-  json data = json::parse(ifs);
-  pyro_connectivity_data pyro(data);
-  tetra_graph graph(pyro);
-
-  // run BFS
-  tetra_tree tree(graph);
-
-
-
-  return 0;
+	return 0;
 }
--- src/tetra_graph_io.cpp
+++ src/tetra_graph_io.cpp
@@ -1,100 +1,48 @@
 #include "tetra_graph_io.hpp"
-#include <sstream>
 #include <set>
 
 using json = nlohmann::json;
 using namespace std;
 
 
-pyro_connectivity_data::pyro_connectivity_data(const json &data) {
+lattice::lattice(const json &data) {
+	/*
 	for (const auto &b : data["bonds"]) {
-		bond_graph.push_back(std::make_pair(b["from_idx"], b["to_idx"]));
+//		bond_graph.push_back(std::make_pair(b["from_idx"], b["to_idx"]));
+		unique_spinids.insert(static_cast<int>(b["from_idx"]));
+		unique_spinids.insert(static_cast<int>(b["to_idx"]));
 	}
+	*/
+
+	std::set<int> unique_spinids;
+	
 	for (const auto &t : data["tetrahedra"]) {
-		int tetra_sl = t["sl"];
-		tetras[tetra_sl].push_back(t["member_spin_idx"]);
+		auto ti = spin_set(t["member_spin_idx"]);
+		unique_spinids.insert(ti.member_spin_ids.begin(), ti.member_spin_ids.end());
+		this->tetras.push_back(std::move(ti));
 	}
 	for (const auto &h : data["rings"]) {
-		hexagons.push_back(h["member_spin_idx"]);
+		auto hi = spin_set(h["member_spin_idx"]);
+		unique_spinids.insert(hi.member_spin_ids.begin(), hi.member_spin_ids.end());
+		this->rings.push_back(std::move(hi));
 	}
+
+	spins.resize(unique_spinids.size());	
+	_register_spins();
 }
 
-
-//std::pair<int,int>
-int pyro_connectivity_data::find_tetra(int tetra_sl, int spin_idx) const {
-	/* returns index pair (tetra_idx, spin_sl) such that
-	 * tetras[tetra_sl][tetra_idx][spin_sl] == spin_idx
-	 *
-	 */
-	assert( (tetra_sl&1) == tetra_sl );
-	// linear search bc we aren't NERDS
-	auto these_tetras = tetras[tetra_sl];
-	for (int tetra_idx=0; tetra_idx<these_tetras.size(); tetra_idx++){
-		auto t = these_tetras[tetra_idx];
-		for (int spin_sl=0; spin_sl<t.size(); spin_sl++){	
-			if (spin_idx == t[spin_sl]){
-				return tetra_idx;
-				//return std::make_pair(tetra_idx, spin_sl);
-			}
+void lattice::_register_spins() {
+	// cross link
+	for (int ti=0; ti<tetras.size(); ti++){
+		for (auto& si : tetras[ti].member_spin_ids){
+			spins[si].tetra_neighbours.push_back(&tetras[ti]);
 		}
 	}
-	std::stringstream s;
-	s << "Spin index "<<spin_idx <<"is not in tetra list";
-	throw std::runtime_error(s.str());
-}
-
-
-
-// a doubly linked list of tetras
-// Responsible for fast index access ONLY
-tetra_graph::tetra_graph(
-		const pyro_connectivity_data& connspec
-		) 
-{
-
-	//allocate all the objects and populate NN list
-	for (int tsl=0; tsl<2; tsl++){
-		auto tlist = connspec.tetras[tsl];
-		for (const auto& member_list : tlist){
-			tetra* t = new tetra;
-			t->sl = tsl;
-
-			for (int spinid : member_list){	
-				int neighbour_idx = connspec.find_tetra(t->sl^1, spinid);
-				tetra* tn = tetra_no[neighbour_idx];
-				t->links.push_back(std::make_pair(
-							spinid, tn));
-			}
-
-			t->idx = tetra_no.size();
-			tetra_no.push_back(t);
+	for (int hi=0; hi < rings.size(); hi++){
+		for (auto& si : rings[hi].member_spin_ids){
+			spins[si].rings_containing.push_back(&rings[hi]);
 		}
 	}
-
-}
-
-int tetra_graph::num_spins() const {
-	std::set<int> spin_ids;
-	for (const auto t : tetra_no){
-		for (auto& l : t->links){
-			spin_ids.insert(l.first);
-		}
-	}
-	return spin_ids.size();
-}
-
-
-
-std::vector<tetra*> tetra_graph::tetras_containing(int spin){
-	std::vector<tetra*> finds;
-	for (auto t : this->tetra_no){
-		for (auto [si, _] : t->links){
-			if (si == spin){
-				finds.push_back(t);
-			}
-		}
-	}
-	return finds;
 }
 
 
