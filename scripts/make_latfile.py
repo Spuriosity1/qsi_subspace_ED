4ab6a60fd2365481199413e969598d3ae6360b55
Spuriosity1 <alaric.sanders@gmail.com>
2025-02-17 22:37:23 +1100

    merged changes

--- scripts/make_latfile.py
+++ scripts/make_latfile.py
@@ -2,26 +2,37 @@ from lattice import Lattice
 import pyrochlore
 import sys
 import os
+import argparse
 
 
-def print_error():
-    print(f"USAGE: {sys.argv[0]} a1,a2,a3_b1,b2,b3_c1,c2,c3 out_dir")
-    sys.exit(1)
+parser = argparse.ArgumentParser(
+    description="Generates a lattice with the specified lattice vectors")
+parser.add_argument("--a1", nargs=3, type=int,
+                    help="First axis of supercell (integers only)")
+parser.add_argument("--a2", nargs=3, type=int,
+                    help="First axis of supercell (integers only)")
+parser.add_argument("--a3", nargs=3, type=int,
+                    help="First axis of supercell (integers only)")
 
+parser.add_argument("out_dir", type=str,
+                    help="path to output directory (name is automatic)")
 
-if len(sys.argv) != 3:
-    print_error()
+parser.add_argument("--delete_sites", type=int, nargs="+",
+                    help="indices of sites to yeeto deleto")
 
-cellspec = [[int(y) for y in x.split(",")]
-            for x in sys.argv[1].split("_")]
+parser.add_argument("--visualise", default=False, type=bool, 
+                    action="store_true", 
+                    help="visualise the constructed lattice before saving")
+
+args = parser.parse_args()
+
+cellspec = [args.a1, args.a2, args.a3]
 
 name = "_".join(["%d,%d,%d" % tuple(a) for a in cellspec])
-if name != sys.argv[1]:
-    print_error()
 
 lat = Lattice(pyrochlore.primitive, cellspec)
 print(f"Modified unit cell: {lat.primitive.lattice_vectors}")
 print("Periodicity: %d %d %d" % tuple(lat.periodicity))
 
-pyrochlore.export_json(lat, os.path.join(sys.argv[2], "pyro_"+name+".json"))
+pyrochlore.export_json(lat, os.path.join(args.out_dir, "pyro_"+name+".json"))
 
