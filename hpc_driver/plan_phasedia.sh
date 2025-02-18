ff6cd1af947f07fe3f7c7a0e11d910f4417e8eeb
Alaric Sanders <als217@cam.ac.uk>
2025-02-18 03:12:23 +0000

    done

--- hpc_driver/plan_phasedia.sh
+++ hpc_driver/plan_phasedia.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
 
-#lattice="pyro_1_3_6x0,-20,-4b4,20,0b0,-8,0b1"
-#lattice="pyro_1_3_6x-12,-4,-8b0,-4,-4b4,0,4b1"
-lattice="pyro_1_3_3x0,4,4b4,0,4b4,4,0b1"
+# Check if the user provided an input file
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <latspec>"
+    exit 1
+fi
+
+lattice="$1"
+
+sector_decomp="../basis_partitions/$lattice"
+
+if [ ! -d "$sector_decomp" ]; then
+	echo "Error: sector decomposition not found at $sector_decomp"
+	exit 1
+fi
 
 latfile="../lattice_files/$lattice.json"
 
-rm phase_dia.plan
 
-for p in `ls ../basis_partitions/$lattice`; do
-       	echo "python3 ../scripts/phase_dia.py $latfile -x -3 -X 3 -d 0.1 --db_path ../../ed_data/results.db --basis_file ../basis_partitions/$lattice/$p" >> phase_dia.plan
+for p in `ls $sector_decomp`; do
+       	echo "python3 ../scripts/phase_dia.py $latfile -x -3 -X 3 -d 0.1 --db_path ../../ed_data/results.db --basis_file ../basis_partitions/$lattice/$p"
 done
