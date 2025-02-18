6dbf3cace69af7f7cdb5899e798afcdbdb18d1bb
Alaric Sanders <als217@cam.ac.uk>
2025-02-18 06:38:00 +0000

    better phase dia planning

--- hpc_driver/plan_phasedia.sh
+++ hpc_driver/plan_phasedia.sh
@@ -6,7 +6,7 @@ if [ "$#" -ne 1 ]; then
     exit 1
 fi
 
-lattice="$1"
+lattice=`basename $1`
 
 sector_decomp="../basis_partitions/$lattice"
 
@@ -19,5 +19,6 @@ latfile="../lattice_files/$lattice.json"
 
 
 for p in `ls $sector_decomp`; do
-       	echo "python3 ../scripts/phase_dia.py $latfile -x -3 -X 3 -d 0.1 --db_path ../../ed_data/results.db --basis_file ../basis_partitions/$lattice/$p"
+       	echo "python3 ../scripts/phase_dia.py $latfile -x -3 -X 0 -d 0.1 --db_repo ../../ed_data/ --basis_file ../basis_partitions/$lattice/$p"
+       	echo "python3 ../scripts/phase_dia.py $latfile -x -0 -X 3 -d 0.1 --db_repo ../../ed_data/ --basis_file ../basis_partitions/$lattice/$p"
 done
