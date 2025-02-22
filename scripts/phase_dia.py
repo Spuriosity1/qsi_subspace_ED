1b5adff09b84847a2f2f15f28eddbf57cbb26566
Spuriosity1 <alaric.sanders@gmail.com>
2025-02-22 13:43:12 +1100

    better rerun logic

--- scripts/phase_dia.py
+++ scripts/phase_dia.py
@@ -25,9 +25,6 @@ ap.add_argument("--db_repo", type=str, default="./",
                 help="Directory to store results in")
 ap.add_argument("--index", type=int, default=time.time_ns())
 ap.add_argument("--krylov_dim", type=int, default=200)
-a = ap.parse_args()
-
-lat = pyrochlore.import_json(a.lattice_file)
 
 
 g_111_dict = {
@@ -45,97 +42,128 @@ g_110_dict = {
         'Z': lambda x : np.array([1, 1, x, x])
         }
 
-g_111 = g_111_dict[a.rotation]
-g_110 = g_110_dict[a.rotation]
 
 
-latvecs = rotation_matrices[a.rotation] @ np.array(lat.lattice_vectors)
+def calc_x_list(args):
+    n_points = int(np.ceil((args.max_x - args.min_x) / args.x_step))
 
-rfh = RingflipHamiltonian(lat)
-print("Setting up basis...")
-if a.basis_file is None:
-    bfile = rfh.basisfile_loc
-else:
-    bfile = a.basis_file
-rfh.load_basis(bfile)
-
-
-R3 = np.sqrt(3)
-R2 = np.sqrt(2)
-
-
-DB_FILE = os.path.join(a.db_repo, f"results_{a.index}.db")
-
-if os.path.isfile(DB_FILE):
-    raise IOError("DB file already exists!")
-
-con = connect_npsql(DB_FILE, timeout=60)
-init_db(con)
-
-n_points = int(np.ceil((a.max_x - a.min_x) / a.x_step))
-
-SPACINGS = {
-        "linear": np.arange(a.min_x, a.max_x, a.x_step),
-        "log": np.logspace(a.min_x, a.max_x, n_points),
+    SPACINGS = {
+        "linear": np.arange(args.min_x, args.max_x, args.x_step),
+        "log": np.logspace(args.min_x, args.max_x, n_points),
         "tanh": np.tan(
-             np.linspace(np.atan(a.min_x),np.atan(a.max_x), n_points)
+             np.linspace(np.atan(args.min_x), np.atan(args.max_x), n_points)
             )
         }
 
 
-x_list = SPACINGS[a.spacing]
-print(x_list)
+    return SPACINGS[args.spacing]
 
 
-for sector in rfh.sectors:
-    print("SECTOR: " + str(sector))
+tol=1e-4
 
-    print("111 field:")
-    for x in tqdm(x_list):
-        for sign in [-1, 1]:
 
-        #    cursor = con.cursor()
-        #    cursor.execute("""SELECT g0_g123 FROM field_111 WHERE g0_g123=? AND g123_sign=? AND latvecs=? AND sector=?
-        #    """, (x, sign, latvecs, str(sector)))
-        #    if len(cursor.fetchall()) != 0:
-        #        print("WARN: skipping duplicate at g0_g123={x}")
-        #        cursor.close()
-        #        continue
-        #    cursor.close()
+def has_111_entry(x, sign, latvecs, sector):
+    cursor = con.cursor()
+    cursor.execute("""SELECT g0_g123 FROM field_111 WHERE
+                    g0_g123 BETWEEN ? AND ?
+                   AND g123_sign=? AND latvecs=? AND sector=?
+    """, (x-tol, x+tol, sign, latvecs, str(sector))
+    )
+    res = cursor.fetchall() != 0
+    cursor.close()
+    return res
 
-            r111 = calc_ring_exp_vals(rfh, g=sign*g_111(x),
-                                      sector=sector, krylov_dim=a.krylov_dim)
 
-            cursor = con.cursor()
-            cursor.execute("""INSERT INTO field_111 (g0_g123, g123_sign, latvecs, sector,
-                                                     edata,
-                                                     expO0, expO1, expO2, expO3)
-                           VALUES (?,?,?,?,?,?,?,?,?);""", (x, sign, latvecs, str(sector), r111[0], *r111[1].values()))
-            cursor.close()
-            con.commit()
+def has_110_entry(x, sign, latvecs, sector):
 
-    print("110 field:")
-    for x in tqdm(x_list):
-        for sign in [-1, 1]:
-        #    cursor = con.cursor()
-        #    cursor.execute("""SELECT g01_g23 FROM field_110 WHERE g01_g23=? AND g23_sign=? AND latvecs=? AND sector=?
-        #    """, (x, sign, latvecs, str(sector)))
-        #    if len(cursor.fetchall()) != 0:
-        #        print("WARN: skipping duplicate at g01_g23={x}")
-        #        cursor.close()
-        #        continue
-        #    cursor.close()
+    cursor = con.cursor()
+    cursor.execute("""SELECT g01_g23 FROM field_110 WHERE
+                    g01_g23 BETWEEN ? AND ?
+                   AND g23_sign=? AND latvecs=? AND sector=?
+    """, (x-tol, x+tol, sign, latvecs, str(sector))
+    )
+    res = cursor.fetchall() != 0
+    cursor.close()
+    return res
 
-            r110 = calc_ring_exp_vals(rfh, g=sign*g_110(x),
-                                      sector=sector, krylov_dim = a.krylov_dim)
-            
-            cursor = con.cursor()
-            cursor.execute("""INSERT INTO field_110 (g01_g23, g23_sign, latvecs, sector,
-                                                     edata,
-                                                     expO0, expO1, expO2, expO3)
-                           VALUES (?,?,?,?,?,?,?,?,?);""", (x, sign, latvecs, str(sector), r110[0], *r110[1].values()))
 
-            cursor.close()
-            con.commit()
+if __name__ == "__main__":
 
-            # save the data
+    a = ap.parse_args()
+
+    lat = pyrochlore.import_json(a.lattice_file)
+    g_111 = g_111_dict[a.rotation]
+    g_110 = g_110_dict[a.rotation]
+
+
+    latvecs = rotation_matrices[a.rotation] @ np.array(lat.lattice_vectors)
+
+    rfh = RingflipHamiltonian(lat)
+    print("Setting up basis...")
+    if a.basis_file is None:
+        bfile = rfh.basisfile_loc
+    else:
+        bfile = a.basis_file
+    rfh.load_basis(bfile)
+
+
+    R3 = np.sqrt(3)
+    R2 = np.sqrt(2)
+
+
+    DB_FILE = os.path.join(a.db_repo, f"results_{a.index}.db")
+
+    initialise=True
+    if os.path.isfile(DB_FILE):
+        print("WARN: db already exists!")
+        initialise=False
+
+    con = connect_npsql(DB_FILE, timeout=60)
+    if initialise:
+        init_db(con)
+
+    x_list = calc_x_list(a)
+    print(x_list)
+
+    for sector in rfh.sectors:
+        print("SECTOR: " + str(sector))
+
+        print("111 field:")
+        for x in tqdm(x_list):
+            for sign in [-1, 1]:
+                if has_111_entry(x, sign, latvecs, sector):
+                    print(f"WARN: duplicate found at {x}")
+                    continue
+
+
+                r111 = calc_ring_exp_vals(rfh, g=sign*g_111(x),
+                                          sector=sector, krylov_dim=a.krylov_dim)
+
+                cursor = con.cursor()
+                cursor.execute("""INSERT INTO field_111 (g0_g123, g123_sign, latvecs, sector,
+                                                         edata,
+                                                         expO0, expO1, expO2, expO3)
+                               VALUES (?,?,?,?,?,?,?,?,?);""", (x, sign, latvecs, str(sector), r111[0], *r111[1].values()))
+                cursor.close()
+                con.commit()
+
+        print("110 field:")
+        for x in tqdm(x_list):
+            for sign in [-1, 1]:
+                if has_110_entry(x,sign,latvecs,sector):
+                    print(f"WARN: skipping duplicate at g01_g23={x}")
+                    continue
+
+                r110 = calc_ring_exp_vals(rfh, g=sign*g_110(x),
+                                          sector=sector, krylov_dim = a.krylov_dim)
+                
+                cursor = con.cursor()
+                cursor.execute("""INSERT INTO field_110 (g01_g23, g23_sign, latvecs, sector,
+                                                         edata,
+                                                         expO0, expO1, expO2, expO3)
+                               VALUES (?,?,?,?,?,?,?,?,?);""", (x, sign, latvecs, str(sector), r110[0], *r110[1].values()))
+
+                cursor.close()
+                con.commit()
+
+                # save the data
