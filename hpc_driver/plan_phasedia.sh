9e14949f73cafc8c514f7b0a0572466af6d8d7ae
Spuriosity1 <alaric.sanders@gmail.com>
2025-02-23 21:52:02 +1100

    added rotation to plan script

--- hpc_driver/plan_phasedia.sh
+++ hpc_driver/plan_phasedia.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Check if the user provided an input file
-if [ "$#" -lt 2 ]; then
-    echo "Usage: $0 <latspec> <initial_index>"
+if [ "$#" -lt 3 ]; then
+    echo "Usage: $0 <latspec> <initial_index> <rotation=IXYZ>"
     exit 1
 fi
 
@@ -19,13 +19,13 @@ sector_list=`ls $sector_decomp`;
 
 
 latfile="../lattice_files/$lattice.json"
-
+ROTATION="$3"
 
 
 index=$2
 for p in $sector_list; do
 
-	BASE_CMD="python3 ../scripts/phase_dia.py $latfile --db_repo ../../ed_data/ --basis_file ../basis_partitions/$lattice/$p"
+	BASE_CMD="python3 ../scripts/phase_dia.py $latfile --db_repo ../../ed_data/ --basis_file ../basis_partitions/$lattice/$p --rotation $ROTATION"
        	echo $BASE_CMD -x -3 -X 0 -d 0.1 --index $index
         let index+=1
        	echo $BASE_CMD -x -0.1 -X 3 -d 0.1 --index $index
