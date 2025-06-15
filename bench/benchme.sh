c4174bc774edfae8ddd56734ad5c86ea81bfd2d7
ALS217 <als217@cam.ac.uk>
2025-06-15 23:05:30 +0000

    bench tweaks

--- bench/benchme.sh
+++ bench/benchme.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-for j in `seq 2 8`; do
+for j in `seq 2 10`; do
     start=`perl -MTime::HiRes=time -e 'printf "%.9f\n", time'`
-	build/gen_spinon_basis bench/test_data/pyro_1,0,0_0,2,0_0,0,$j.json > /dev/null
+	build/gen_spinon_basis bench/test_data/pyro_1,0,0_0,2,0_0,0,$j.json --n_threads $1 > /dev/null
     end=`perl -MTime::HiRes=time -e 'printf "%.9f\n", time'`
 	runtime=$(awk -v s="$start" -v e="$end" 'BEGIN { printf "%.6f\n", e - s }')
     echo "$runtime"
