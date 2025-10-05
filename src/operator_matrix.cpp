152aedd6acb002446de2ea90b5e1d07e7b084da1
Spuriosity1 <alaric.sanders@gmail.com>
2025-10-05 17:48:26 +0100

    better OMP parallelism

--- src/operator_matrix.cpp
+++ src/operator_matrix.cpp
@@ -1,17 +1,31 @@
 #include "operator_matrix.hpp"
+#include <omp.h>
 
 // performs y <- Ax + y
 template <RealOrCplx coeff_t>
 void LazyOpSum<coeff_t>::evaluate_add(const coeff_t* x, coeff_t* y) const {
     for (const auto& [c, op] : ops.terms) {
         //op.apply_add(basis, x, y, c);
-        
-        #pragma omp parallel for schedule(static)
-        for (ZBasis::idx_t i = 0; i < basis.dim(); ++i) {
-            ZBasis::idx_t J = i;
-            coeff_t sign = x[i] * static_cast<double>(op.applyIndex(basis, J));
+        #pragma omp parallel
+        {
+            std::vector<std::pair<ZBasis::idx_t, coeff_t>> local_updates;
+            local_updates.reserve(basis.dim() / omp_get_num_threads() + 64);
 
-            y[J] += sign*c;
+            #pragma omp for schedule(static) nowait
+            for (ZBasis::idx_t i = 0; i < basis.dim(); ++i) {
+                ZBasis::idx_t J = i;
+                coeff_t dy = c * x[i] * static_cast<double>(op.applyIndex(basis, J));
+                local_updates.emplace_back(J, dy);
+            }
+
+            // Each thread writes its results once - minimal contention
+            #pragma omp critical
+            {
+                for (const auto& [idx, val] : local_updates) {
+                    y[idx] += val;
+                }
+            }
+            
         }
     }
 }
